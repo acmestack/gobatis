9778170fa9d1652e1d6056dc74285c9061a1bae8
Qicz <qiczzhu@gmail.com>
2022-08-02 15:38:58 +0800

    update license template (#22)

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2022, AcmeStack
- * All rights reserved.
- *
+ * Licensed to the AcmeStack under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
--- reflection/parseparam.go
+++ reflection/parseparam.go
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2022, AcmeStack
- * All rights reserved.
- *
+ * Licensed to the AcmeStack under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF interface{} KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
